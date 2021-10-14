e3426acb5925bcedca19d03268073c6d7abe3199
ZhangJY0426 <atguigu0426@gg.com>
2021-10-14 10:56:46 +0800

    day04-上午

---
+++ src/main/java/com/atguigu/bean/MarketingUserBehavior.java
@@ -0,0 +1,14 @@
+package com.atguigu.bean;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarketingUserBehavior {
+    private Long userId;
+    private String behavior;
+    private String channel;
+    private Long timestamp;
+}
